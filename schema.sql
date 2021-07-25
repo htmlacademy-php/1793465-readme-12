8539aa1dac48e086e1024e213b169c408a339015
resurt <resurt@yahoo.com>
2021-07-25 18:11:20 +0300

    Создание схемы БД

--- schema.sql
+++ schema.sql
@@ -7,31 +7,31 @@ USE readme_db;
 CREATE TABLE `users` (
                         `id` INT(11) NOT NULL AUTO_INCREMENT ,
                         `reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-                        `email` CHAR(50) NOT NULL ,
-                        `login` CHAR(50) NOT NULL ,
+                        `email` VARCHAR(50) NOT NULL ,
+                        `login` VARCHAR(50) NOT NULL ,
                         `password` CHAR(64) NOT NULL , PRIMARY KEY (`id`),
-                        `avatar_link` CHAR(100) NULL
+                        `avatar_link` VARCHAR(100) NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE `posts` (
                         `id` INT(11) NOT NULL AUTO_INCREMENT ,
                         `pub_date_post` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-                        `title` CHAR(50) NOT NULL ,
-                        `text` VARCHAR(2000) NOT NULL ,
-                        `author_quote` CHAR(100) NOT NULL ,
-                        `image_link` CHAR(100) NULL ,
-                        `video_link` CHAR(100) NOT NULL ,
-                        `site_link` CHAR(100) NOT NULL ,
-                        `views` INT(11) NOT NULL ,
+                        `title` VARCHAR(50) NOT NULL ,
+                        `text` TEXT(2000) NOT NULL ,
+                        `author_quote` VARCHAR(100) NOT NULL ,
+                        `image_link` VARCHAR(100) NULL ,
+                        `video_link` VARCHAR(100) NOT NULL ,
+                        `site_link` VARCHAR(100) NOT NULL ,
+                        `views` INT(11) NOT NULL DEFAULT 0,
                         `user_id` INT(11) NOT NULL ,
-                        `content_type_id` CHAR(50) NOT NULL ,
-                        `hashtag_id` CHAR(50) NULL
+                        `content_type_id` INT(11) NOT NULL ,
+                        `hashtag_id` INT(11) NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE `comments` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
                          `comment_pub_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-                         `comment_content` CHAR(100) NOT NULL ,
+                         `comment_content` TEXT(100) NOT NULL ,
                          `user_id` INT(11) NOT NULL ,
                          `post_id` INT(11) NOT NULL
 ) ENGINE = InnoDB;
@@ -51,18 +51,18 @@ CREATE TABLE `subscriptions` (
 CREATE TABLE `messages` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
                          `date_message_send` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-                         `text_message` VARCHAR(2000) NOT NULL ,
+                         `text_message` TEXT(2000) NOT NULL ,
                          `user_id_sender` INT(11) NOT NULL ,
                          `user_id_recipient` INT(11) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE `hashtags` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
-                         `hashtag_name` CHAR(50) NOT NULL
+                         `hashtag_name` VARCHAR(50) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE `content_type` (
                           `id` INT(11) NOT NULL AUTO_INCREMENT ,
-                          `type_name` CHAR(50) NOT NULL ,
-                          `class_name` CHAR(50) NOT NULL
+                          `type_name` VARCHAR(50) NOT NULL ,
+                          `class_name` VARCHAR(50) NOT NULL
 ) ENGINE = InnoDB;
