4e4124e1cc78852a20a54c023f192353268bcea0
resurt <resurt@yahoo.com>
2021-08-01 20:03:02 +0300

    Написание SQL-запросов

---
+++ queries.sql
@@ -0,0 +1,43 @@
+INSERT INTO `content_type` (`type_name`, `class_name`)
+VALUES  ('Текст', 'text'),
+        ('Цитата', 'quote'),
+        ('Картинка', 'photo'),
+        ('Видео', 'video'),
+        ('Ссылка', 'link');
+
+INSERT INTO `users` (`email`, `login`, `password`)
+VALUES  ('htmlacademy_larisa@gmail.com', 'Лариса', '1234556'),
+        ('vladik_php@yandex.ru', 'Владик', '8675432'),
+        ('viktor-191923@mail.ru', 'Виктор', '7683542'  );
+
+INSERT INTO `comments` (`comment_content`, `user_id`, `post_id`)
+VALUES  ('Если вы знаете, что это такое, то у вас уже есть пенсионное удостоверение', '1', '1'),
+        ('Кругом обман', '2', '2');
+
+INSERT INTO `posts` (`title`, `text`, `author_quote`, `image_link`, `video_link`, `site_link`, `views`, `user_id`, `content_type_id`, `hashtag_id`)
+VALUES  ('Цитата', 'Мы в жизни любим только раз, а после ищем лишь похожих','Неизвестный Автор', NULL, NULL, NULL, '10', '1', '2', NULL),
+        ('Игра престолов', 'Не могу дождаться начала финального сезона своего любимого сериала!
+        Не могу дождаться начала финального сезона своего любимого сериала!
+        Не могу дождаться начала финального сезона своего любимого сериала!
+        Не могу дождаться начала финального сезона своего любимого сериала!
+        Не могу дождаться начала финального сезона своего любимого сериала!
+        Не могу дождаться начала финального сезона своего любимого сериала!', NULL, NULL, NULL, NULL, '15', '2', '1', NULL ),
+       ('Наконец, обработал фотки!', NULL, NULL, 'rock-medium.jpg', NULL, NULL, '4', '3', '3', NULL),
+       ('Моя мечта', NULL, NULL, 'coast-medium.jpg', NULL, NULL, '1', '1', '3', NULL),
+       ('Лучшие курсы', NULL, NULL, NULL, NULL, 'www.htmlacademy.ru', '33', '2', '5', NULL);
+
+/* получить список постов с сортировкой по популярности и вместе с именами авторов и типом контента; */
+SELECT * FROM `posts` ORDER BY `posts`.`views` DESC;
+
+/* получить список комментариев для одного поста, в комментариях должен быть логин пользователя; */
+SELECT * FROM `posts` WHERE `user_id` = 1;
+
+/* добавить лайк к посту; */
+INSERT INTO `likes` (`user_id`, `post_id`)
+VALUES  ('1', '2'),
+        ('2', '1');
+
+/* подписаться на пользователя. */
+INSERT INTO `subscriptions` (`user_id_subscribed`, `user_id_subscribed_to`)
+VALUES  ('2', '1'),
+        ('3', '1');
--- schema.sql
+++ schema.sql
@@ -1,68 +1,76 @@
-CREATE DATABASE readme_db
+CREATE DATABASE IF NOT EXISTS readme_db
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
 USE readme_db;
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
                         `id` INT(11) NOT NULL AUTO_INCREMENT ,
                         `reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
                         `email` VARCHAR(50) NOT NULL ,
                         `login` VARCHAR(50) NOT NULL ,
-                        `password` CHAR(64) NOT NULL , PRIMARY KEY (`id`),
-                        `avatar_link` VARCHAR(100) NULL
+                        `password` CHAR(64) NOT NULL ,
+                        `avatar_link` VARCHAR(100) NULL,
+                        PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `posts` (
+CREATE TABLE IF NOT EXISTS `posts` (
                         `id` INT(11) NOT NULL AUTO_INCREMENT ,
                         `pub_date_post` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
                         `title` VARCHAR(50) NOT NULL ,
-                        `text` TEXT(2000) NOT NULL ,
-                        `author_quote` VARCHAR(100) NOT NULL ,
+                        `text` TEXT(2000) NULL ,
+                        `author_quote` VARCHAR(100) NULL ,
                         `image_link` VARCHAR(100) NULL ,
-                        `video_link` VARCHAR(100) NOT NULL ,
-                        `site_link` VARCHAR(100) NOT NULL ,
+                        `video_link` VARCHAR(100) NULL ,
+                        `site_link` VARCHAR(100) NULL ,
                         `views` INT(11) NOT NULL DEFAULT 0,
                         `user_id` INT(11) NOT NULL ,
                         `content_type_id` INT(11) NOT NULL ,
-                        `hashtag_id` INT(11) NULL
+                        `hashtag_id` INT(11) NULL,
+                         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `comments` (
+CREATE TABLE IF NOT EXISTS `comments` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
                          `comment_pub_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
                          `comment_content` TEXT(100) NOT NULL ,
                          `user_id` INT(11) NOT NULL ,
-                         `post_id` INT(11) NOT NULL
+                         `post_id` INT(11) NOT NULL,
+                         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `likes` (
+CREATE TABLE IF NOT EXISTS `likes` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
                          `user_id` INT(11) NOT NULL ,
-                         `post_id` INT(11) NOT NULL
+                         `post_id` INT(11) NOT NULL,
+                         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `subscriptions` (
+CREATE TABLE IF NOT EXISTS `subscriptions` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
                          `user_id_subscribed` INT(11) NOT NULL ,
-                         `user_id_subscribed_to` INT(11) NOT NULL
+                         `user_id_subscribed_to` INT(11) NOT NULL,
+                         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `messages` (
+CREATE TABLE IF NOT EXISTS `messages` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
                          `date_message_send` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
                          `text_message` TEXT(2000) NOT NULL ,
                          `user_id_sender` INT(11) NOT NULL ,
-                         `user_id_recipient` INT(11) NOT NULL
+                         `user_id_recipient` INT(11) NOT NULL,
+                         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `hashtags` (
+CREATE TABLE IF NOT EXISTS `hashtags` (
                          `id` INT(11) NOT NULL AUTO_INCREMENT ,
-                         `hashtag_name` VARCHAR(50) NOT NULL
+                         `hashtag_name` VARCHAR(50) NOT NULL,
+                         PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `content_type` (
+CREATE TABLE IF NOT EXISTS `content_type` (
                           `id` INT(11) NOT NULL AUTO_INCREMENT ,
                           `type_name` VARCHAR(50) NOT NULL ,
-                          `class_name` VARCHAR(50) NOT NULL
+                          `class_name` VARCHAR(50) NOT NULL,
+                          PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
